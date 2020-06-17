1718837d8fe3b6fdc3cb44126e77d5ac5d10eda1
Edgar Justavino <edjust@Edgars-MacBook-Pro.local>
2020-06-17 13:53:34 -0300

    App Ecoleta

---
+++ server/src/controllers/itemsController.ts
@@ -0,0 +1,22 @@
+import knex from "../database/connection";
+import { Request, Response } from "express";
+
+class ItemsController {
+  async index(request: Request, response: Response) {
+    // SELECT * FROM items
+    const items = await knex("items").select("*");
+
+    // Serialização / API Transform
+    const serializedItems = items.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        image_url: `http://192.168.0.19:3333/uploads/${item.image}`,
+      };
+    });
+
+    return response.json(serializedItems);
+  }
+}
+
+export default ItemsController;
